e403d90e56fd7e4b36dc2e24873102546a28d082
Kal <kalvinsev@gmail.com>
2023-04-10 16:40:31 -0700

    front end components, client side rendering, graphql data retreiving imp.

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,12 +1,21 @@
 
-import { use, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
-import { themeDark, themeLight } from 'lib/theme';
+import { themeDark, themeLight } from '../lib/theme';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../lib/apollo';
+import { AuthProvider } from '../lib/useAuth';
+
 
 
 export default function MyApp ({ Component, pageProps }) {
-    
+    const apolloClient = useApollo(pageProps.initialApolloState);
+    const [darkState, setDarkState] = useState(false);
+    const handleThemeChange = () => {
+        setDarkState(!darkState);
+    }
+
     useEffect(() => {
         //Remove the servier-side  injected css
         const jssStyles = document.querySelector("#jss-server-side");
@@ -16,9 +25,13 @@ export default function MyApp ({ Component, pageProps }) {
     }, []);
 
     return (
-        <ThemeProvider theme={false ? themeDark : themeLight}>
-            <CssBaseline />
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <ApolloProvider client={apolloClient}>
+            <ThemeProvider theme={darkState ? themeDark : themeLight}>
+                <CssBaseline />
+                <AuthProvider>
+                    <Component {...pageProps} />
+                </AuthProvider>
+            </ThemeProvider>
+        </ApolloProvider>
     );
 }
